290263ebe22b9720bdff09bbac6bea198b7a969c
Prabhjot Singh <35163500+resurrected21@users.noreply.github.com>
2019-05-29 11:56:02 +0530

    Create lineage.mk

---
+++ lineage.mk
@@ -0,0 +1,14 @@
+# Copyright (C) 2019 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+$(call inherit-product, device/samsung/on5xelte/full_on5xelte.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_on5xelte
+PRODUCT_DEVICE := on5xelte
+PRODUCT_MODEL := G570F
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
